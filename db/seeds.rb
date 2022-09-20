731d85d0121d9d95b735e052a7f59e41af0b1864
jr-cast <castanos@pm.me>
2022-09-20 18:40:52 -0500

    Add table, model and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Message.create(message: "Hiya!, Good morning API!")
+Message.create(message: "Howdy API!, Good morning!")
+Message.create(message: "Howdy neighbor!, Good API you go there!")
+Message.create(message: "Top of the API to ya'!")
+Message.create(message: "Good Moorning APIII!")
